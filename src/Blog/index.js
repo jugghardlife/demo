802a59ef396c40c474f1cf81171a5617c782df7d
jugghardlife <601493614@qq.com>
2016-12-03 11:10:06 +0800

    demo

--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 import BlogCard from "../component/BlogCard";
-let data=
+let data=[
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"},
+  {index:1,title:"hello",desc:"world"}
+]
 
 class Blog extends React.Component{
   render(){
